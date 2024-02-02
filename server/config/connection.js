1773c4766a63cfb338ed215143f9d881f9df681d
chilejay7 <codyburk7@yahoo.com>
2024-02-01 20:54:13 -0700

    connection.js defined

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,11 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || '');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/justeatit')
+    .then(() => {
+        console.log(`The database is up and connected.`);
+    })
+    .catch((err) => {
+        console.error(err);
+    });
 
 module.exports = mongoose.connection;
