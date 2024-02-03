fa28724fdeb0b487551704365c9d7d4611185cf1
Brandon Wing <brandon.wing245@gmail.com>
2024-02-03 12:45:14 -0700

    Added Sign Up Page form.

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,7 +1,10 @@
+import SignUpForm from '../components/SignUpForm';
+
 export default function SignUp() {
   return (
     <div className="container">
       <h2>Sign Up Page</h2>
+      <SignUpForm />
     </div>
   );
 }
