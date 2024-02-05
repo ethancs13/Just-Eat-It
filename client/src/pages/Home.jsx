28fbf8ce74bd572f1a4f51efa58cb3475f5d7480
Lacey Trokey <trokeyln@gmail.com>
2024-02-05 11:31:58 -0700

    Added search bar

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,11 @@
+import HomeSearchBar from "../components/SearchBar";
+
 export default function Home() {
   return (
     <div className="bg">
-      <div className="container">{/* Add search bar */}</div>
+      <div className="container">
+        <HomeSearchBar />
+      </div>
     </div>
   );
 }
