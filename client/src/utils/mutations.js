504c5c6a2dc432685b48b86217115ddbd3811eea
Brandon Wing <brandon.wing245@gmail.com>
2024-02-06 10:56:59 -0700

    Updated mutations for adding cuisine and creating restaurant.

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,4 +1,4 @@
-import { gql } from "@apollo/client";
+import { gql } from '@apollo/client';
 
 export const CREATE_USER = gql`
   mutation createUser($username: String!, $password: String!) {
@@ -13,38 +13,42 @@ export const CREATE_USER = gql`
 `;
 
 export const LOGIN = gql`
-    mutation login($username: String!, $password: String!) {
-        login(username: $username, password: $password) {
-            token
-            user {
-                _id
-                username
-            }
-        }
+  mutation login($username: String!, $password: String!) {
+    login(username: $username, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
     }
+  }
 `;
 
 export const ADD_CUISINE = gql`
-    mutation addCuisine($cuisineData: CuisineInput!) {
-      addCuisine(cuisineData: $cuisineData) {
-        _id
-        username
-        cuisine {
-          _id
-          name
-        }
-      }
+  mutation addCuisine($cuisineData: CuisineInput!) {
+    addCuisine(cuisineData: $cuisineData) {
+      _id
+      username
     }
-`
-export const REMOVE_CUISINE = gql`
-    mutation removeCuisine($cuisineData: CuisineInput!) {
-      removeCuisine(cuisineData: $cuisineData) {
-        _id
-        username
-        cuisine {
-          _id
-          name
-        }
-      }
-    }
-`
+  }
+`;
+
+export const CREATE_RESTAURANT = gql`
+  mutation createRestaurant(
+    $businessId: String!
+    $name: String!
+    $rating: Float!
+    $image: String
+    $url: String
+    $location: String
+  ) {
+    createRestaurant(
+      businessId: $businessId
+      name: $name
+      rating: $rating
+      image: $image
+      url: $url
+      location: $location
+    )
+  }
+`;
