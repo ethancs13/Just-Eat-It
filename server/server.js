dbf166955b877ee1460273b00c49f45395cc5384
Lacey Trokey <trokeyln@gmail.com>
2024-02-05 17:16:57 -0700

    minor tweaks for the .env file

--- server/server.js
+++ server/server.js
@@ -1,15 +1,15 @@
-const express = require('express');
-require('dotenv').config();
-const axios = require('axios');
-const cors = require('cors');
-const { ApolloServer } = require('@apollo/server');
-const { expressMiddleware } = require('@apollo/server/express4');
-const path = require('path');
+const express = require("express");
+require("dotenv").config();
+const axios = require("axios");
+const cors = require("cors");
+const { ApolloServer } = require("@apollo/server");
+const { expressMiddleware } = require("@apollo/server/express4");
+const path = require("path");
 
-const { authMiddleware } = require('./utils/auth');
+const { authMiddleware } = require("./utils/auth");
 
-const { typeDefs, resolvers } = require('./schemas');
-const db = require('./config/connection');
+const { typeDefs, resolvers } = require("./schemas");
+const db = require("./config/connection");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -21,27 +21,25 @@ const server = new ApolloServer({
   resolvers,
 });
 
-// Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async () => {
   await server.start();
 
   app.use(express.urlencoded({ extended: false }));
   app.use(express.json());
 
-  // Important for MERN Setup: Any client-side requests that begin with '/graphql' will be handled by our Apollo Server
   app.use(
-    '/graphql',
+    "/graphql",
     expressMiddleware(server, {
       context: authMiddleware,
     })
   );
 
-  app.get('/', async (req, res) => {
+  app.get("/", async (req, res) => {
     const { location, term } = req.query;
 
     try {
       const response = await axios.get(
-        'https://api.yelp.com/v3/businesses/search',
+        "https://api.yelp.com/v3/businesses/search",
         {
           headers: {
             Authorization: `Bearer ${process.env.API_KEY}`,
@@ -63,18 +61,15 @@ const startApolloServer = async () => {
     }
   });
 
-  // Important for MERN Setup: When our application runs from production, it functions slightly differently than in development
-  // In development, we run two servers concurrently that work together
-  // In production, our Node server runs and delivers our client-side bundle from the dist/ folder
-  if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/dist')));
+  if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../client/dist")));
 
-    app.get('*', (req, res) => {
-      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+    app.get("*", (req, res) => {
+      res.sendFile(path.join(__dirname, "../client/dist/index.html"));
     });
   }
 
-  db.once('open', () => {
+  db.once("open", () => {
     app.listen(PORT, () => {
       console.log(`API server running on port ${PORT}!`);
       console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
@@ -82,5 +77,4 @@ const startApolloServer = async () => {
   });
 };
 
-// Call the async function to start the server
 startApolloServer();
