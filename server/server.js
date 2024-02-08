9103ad70a0427d9bb74e74f2546c811bd789145c
Brandon Wing <brandon.wing245@gmail.com>
2024-02-07 22:33:11 -0700

    Changed wording on api request for better readability.

--- server/server.js
+++ server/server.js
@@ -1,15 +1,15 @@
-const express = require('express');
-require('dotenv').config();
-const axios = require('axios');
-const cors = require('cors');
-const { ApolloServer } = require('@apollo/server');
-const { expressMiddleware } = require('@apollo/server/express4');
-const path = require('path');
+const express = require("express");
+require("dotenv").config();
+const axios = require("axios");
+const cors = require("cors");
+const { ApolloServer } = require("@apollo/server");
+const { expressMiddleware } = require("@apollo/server/express4");
+const path = require("path");
 
-const { authMiddleware } = require('./utils/auth');
+const { authMiddleware } = require("./utils/auth");
 
-const { typeDefs, resolvers } = require('./schemas');
-const db = require('./config/connection');
+const { typeDefs, resolvers } = require("./schemas");
+const db = require("./config/connection");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -28,25 +28,25 @@ const startApolloServer = async () => {
   app.use(express.json());
 
   app.use(
-    '/graphql',
+    "/graphql",
     expressMiddleware(server, {
       context: authMiddleware,
     })
   );
 
-  app.get('/', async (req, res) => {
-    const { location, term } = req.query;
+  app.get("/", async (req, res) => {
+    const { location, cuisine } = req.query;
 
     try {
       const response = await axios.get(
-        'https://api.yelp.com/v3/businesses/search',
+        "https://api.yelp.com/v3/businesses/search",
         {
           headers: {
             Authorization: `Bearer ${process.env.API_KEY}`,
           },
           params: {
             location: location,
-            term: term,
+            cuisine: cuisine,
             limit: 12,
           },
         }
@@ -61,15 +61,15 @@ const startApolloServer = async () => {
     }
   });
 
-  if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/dist')));
+  if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../client/dist")));
 
-    app.get('*', (req, res) => {
-      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+    app.get("*", (req, res) => {
+      res.sendFile(path.join(__dirname, "../client/dist/index.html"));
     });
   }
 
-  db.once('open', () => {
+  db.once("open", () => {
     app.listen(PORT, () => {
       console.log(`API server running on port ${PORT}!`);
       console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
