5f8fa9a74edbf9931f7d7d4175aad754ca2bbce9
Brandon Wing <brandon.wing245@gmail.com>
2024-02-08 20:51:39 -0700

    Fixed restaurant search and favorites page buttons.

--- client/src/components/SearchResults/index.jsx
+++ client/src/components/SearchResults/index.jsx
@@ -3,7 +3,11 @@ import RestaurantCard from "../RestaurantCard";
 const SearchResults = ({ results }) => (
   <div className="row card-container">
     {results.map((restaurant) => (
-      <RestaurantCard key={restaurant.id} restaurant={restaurant} />
+      <RestaurantCard
+        key={restaurant.id}
+        restaurant={restaurant}
+        favoritePage={false}
+      />
     ))}
   </div>
 );
