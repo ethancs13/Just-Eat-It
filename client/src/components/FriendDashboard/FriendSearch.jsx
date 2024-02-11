83a2f23c13f1c510d6b38d638d4d952cd5a07732
Lacey Trokey <trokeyln@gmail.com>
2024-02-10 22:11:25 -0700

    Fixed error on handle submit o FriendSearch.jsx

--- client/src/components/FriendDashboard/FriendSearch.jsx
+++ client/src/components/FriendDashboard/FriendSearch.jsx
@@ -3,99 +3,86 @@ import { useLazyQuery } from "@apollo/client";
 import { QUERY_USER_BY_USERNAME } from "../../utils/queries";
 import FriendModal from "./FriendModal";
 
-import {
-    Box,
-    Input,
-    Button,
-    ChakraProvider,
-    Flex,
-} from "@chakra-ui/react";
+import { Box, Input, Button, ChakraProvider, Flex } from "@chakra-ui/react";
 
 const FriendSearch = () => {
+  const [searchFriend, setSearchFriend] = useState({ friendName: "" });
+  const [friendFavorites, setFriendFavorites] = useState([]);
+  const [getUser, { loading, error, data }] = useLazyQuery(
+    QUERY_USER_BY_USERNAME
+  );
 
-    const [searchFriend, setSearchFriend] = useState({ friendName: "" });
-    const [friendFavorites, setFriendFavorites] = useState([]);
-    const [getUser, { loading, error, data }] = useLazyQuery(QUERY_USER_BY_USERNAME);
+  const handleChange = (e) => {
+    const newValue = e.target.value;
+    console.log("New Value:", newValue);
 
-    const handleChange = (e) => {
-        const newValue = e.target.value;
-        console.log('New Value:', newValue);
+    setSearchFriend((currData) => {
+      currData.friendName = newValue;
+      return { ...currData };
+    });
+  };
 
-        setSearchFriend(currData => {
-            currData.friendName = newValue;
-            return { ...currData };
-        })
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setFriendFavorites([]);
+      await getUser({ variables: { username: searchFriend.friendName } });
+
+      setSearchFriend({ friendName: "" });
+      console.log("Friend Foods:", data?.user.savedCuisines);
+    } catch (err) {
+      console.error("Error querying user data:", err);
     }
+  };
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        try {
-            setFriendFavorites([]);
-            await getUser({ variables: { username: searchFriend.friendName } });
-            
-            // const friendFoods = data.user.savedCuisines;
-            setSearchFriend({ friendName: "" });
-            // setFriendFavorites(data.user.savedCuisines);
-            console.log('Friend Foods:', friendFoods);
+  useEffect(() => {
+    if (data) {
+      setFriendFavorites(data.user.savedCuisines || []);
+    }
+  }, [data]);
 
-        } catch (err) {
+  return (
+    <ChakraProvider>
+      <Box className="searchContainer friend-dashboard" p={4}>
+        <Flex align="center" justify="center">
+          <form onSubmit={handleSubmit}>
+            <Flex align="center" justify="center">
+              <Input
+                placeholder="Search for a friend"
+                flex={2}
+                size="lg"
+                color="orange"
+                borderColor="orange"
+                _focus={{ borderColor: "orange.500" }}
+                onChange={handleChange}
+                name="friendName"
+                value={searchFriend.friendName}
+              />
 
-            console.error('Error querying user data:', error);
-        }
+              <Button type="submit" colorScheme="orange" size="lg" ml={2}>
+                Find Friend
+              </Button>
+            </Flex>
+          </form>
+        </Flex>
+      </Box>
 
-    };
+      <div>
+        <p>{data?.user.username} likes:</p>
+        <ul>
+          {data?.user.savedCuisines.map((cuisine) => (
+            <li key={cuisine.name}>{cuisine.name}</li>
+          ))}
+        </ul>
+      </div>
 
-    useEffect(() => {
-        if (data) {
-          setFriendFavorites(data.user.savedCuisines || []);
-        }
-      }, [data]);
-
-    return (
-        <ChakraProvider>
-            <Box className="searchContainer friend-dashboard" p={4}>
-                <Flex align="center" justify="center">
-                    <form onSubmit={(handleSubmit)}>
-                        <Flex align="center" justify="center">
-                            <Input
-                                placeholder="Search for a friend"
-                                flex={2}
-                                size="lg"
-                                color="orange"
-                                borderColor="orange"
-                                _focus={{ borderColor: "orange.500" }}
-                                onChange={handleChange}
-                                name="friendName"
-                                value={searchFriend.friendName}
-                            />
-
-
-                            <Button type="submit" colorScheme="orange" size="lg" ml={2} >
-                                Find Friend
-                            </Button>
-                        </Flex>
-                    </form>
-                </Flex>
-            </Box>
-
-            <div>
-                <p>{data?.user.username} likes:</p>
-                <ul>
-                    {data?.user.savedCuisines.map(cuisine => (
-                        <li key={cuisine.name}>{cuisine.name}</li>
-                    ))}
-                </ul>
-
-            </div>
-
-            <div className="center">
-                {friendFavorites && friendFavorites.length > 0 && (
-                    <FriendModal friendFoods={friendFavorites} />
-                )}
-            </div>
-
-        </ChakraProvider>
-    )
-}
+      <div className="center">
+        {friendFavorites && friendFavorites.length > 0 && (
+          <FriendModal friendFoods={friendFavorites} />
+        )}
+      </div>
+    </ChakraProvider>
+  );
+};
 
 export default FriendSearch;
