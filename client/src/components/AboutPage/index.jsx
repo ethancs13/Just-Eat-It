1b6f634c8295371dd8232c81ed13954ca088108c
Brandon Wing <brandon.wing245@gmail.com>
2024-02-12 11:17:32 -0700

    Added About page and in nav bar.

---
+++ client/src/components/AboutPage/index.jsx
@@ -0,0 +1,16 @@
+import Accordion from "react-bootstrap/Accordion";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+
+const AboutPage = () => {
+  return (
+    <Accordion>
+      <Accordion.Item eventKey="0">
+        <Accordion.Header>What is JustEatIt about?</Accordion.Header>
+        <Accordion.Body>Idk</Accordion.Body>
+      </Accordion.Item>
+    </Accordion>
+  );
+};
+
+export default AboutPage;
