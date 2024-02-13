62647f85207d17643bb929f94fd2f035ba5e6d3a
BuildTools <unconfigured@null.spigotmc.org>
2024-02-12 21:46:13 -0700

    https / at end

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const handleSearch = async (location, cuisine) => {
   try {
-    const response = await axios.get("http://just-eat-it-be3958285291.herokuapp.com", {
+    const response = await axios.get("https://just-eat-it-be3958285291.herokuapp.com/", {
       params: {
         location: location,
         cuisine: cuisine,
@@ -18,7 +18,7 @@ export const handleSearch = async (location, cuisine) => {
 
 export const getRandomRestaurant = async (location, cuisine) => {
   try {
-    const response = await axios.get("http://just-eat-it-be3958285291.herokuapp.com", {
+    const response = await axios.get("https://just-eat-it-be3958285291.herokuapp.com/", {
       params: {
         location: location,
         cuisine: cuisine,
